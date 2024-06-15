e6a5a9aaf64203c86268f4e58da29dc88e6924b2
jayden <46307433+kermeow@users.noreply.github.com>
2024-06-15 03:02:23 +0100

    arghhh

--- mod-loader/gaml/GAML.gd
+++ mod-loader/gaml/GAML.gd
@@ -1,7 +1,53 @@
 extends Node
 
+#func _disable_setter(_value): return
+
+var exec_path = OS.get_executable_path().get_base_dir()
+var gaml_path = exec_path.plus_file("gaml")
+
+func _emergency_exit(reason:String = "Unknown"):
+	push_error("Fatal GAML error: %s" % reason)
+	get_tree().quit()
+func _verify_gaml_files():
+	var error = "Missing file! %s"
+	var dir = Directory.new()
+	if !dir.dir_exists(gaml_path): _emergency_exit(error % "gaml")
+	if !dir.file_exists(gaml_path.plus_file("game.cfg")): _emergency_exit(error % "game.cfg")
+	if !dir.file_exists(gaml_path.plus_file("gaml.cfg")): _emergency_exit(error % "gaml.cfg")
+	return OK
+
 func _enter_tree():
 	get_tree().change_scene("res://gaml/Loading.tscn")
-
 func _ready():
-	print("_ready")
+	call_deferred("_load_game")
+
+func _reinit_node(node: Node, recursive: bool = false):
+	var method = "notification"
+	if recursive: method = "propagate_notification"
+	node.call(method, NOTIFICATION_POSTINITIALIZE)
+	node.call(method, NOTIFICATION_ENTER_TREE)
+	node.call(method, NOTIFICATION_POST_ENTER_TREE)
+	node.call(method, NOTIFICATION_READY)
+
+signal load_game_stage # (message: String)
+signal load_game_finished # ()
+var _is_game_loaded = false
+func _load_game():
+	if _is_game_loaded: return
+	_is_game_loaded = true
+	
+	_verify_gaml_files()
+	
+	var game_cfg = ConfigFile.new()
+	game_cfg.load(gaml_path.plus_file("game.cfg"))
+	
+	for key in game_cfg.get_section_keys("autoload"):
+		var node = get_node("/root/%s" % key)
+		var script_path = game_cfg.get_value("autoload", key).trim_prefix("*")
+		var script = load(script_path)
+		node.set_script(node)
+		call_deferred("_reinit_node", node, false)
+	
+	var main_scene = game_cfg.get_value("application", "run/main_scene")
+	print("Main scene is %s" % main_scene)
+	get_tree().change_scene(main_scene)
