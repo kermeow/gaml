a480859aa1c0cdf8e995f0f57fc1ff2e91af53d5
jayden <46307433+kermeow@users.noreply.github.com>
2024-06-15 02:11:54 +0100

    add scripts

---
+++ scripts/bootstrapper.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# Loads GAML and nothing else
+
+func _init():
+	var dir = Directory.new()
+	var path = OS.get_executable_path().get_base_dir()
+	var gaml_pck = path.plus_file("gaml/GAML.pck")
+	if !dir.file_exists(gaml_pck):
+		print("GAML.pck is missing!")
+		get_tree().quit()
+	ProjectSettings.load_resource_pack(gaml_pck)
+	GAML.set_script("res://gaml/GAML.gd")
+	GAML.bootstrap_finish()
